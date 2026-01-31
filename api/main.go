0f6fc518077605f0371fb9050f46f86e6077a60a
MateusBorsoi <borsoimateus@gmail.com>
2026-01-31 17:02:48 -0300

    login&authMiddleware

--- api/main.go
+++ api/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"beyond/internal/controllers"
 	"beyond/internal/infra/database"
+	"beyond/internal/middlewares"
 	"log"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
@@ -17,6 +20,21 @@ func main() {
 	database.Connect()
 
 	router := gin.Default()
+
+	auth := router.Group("/auth")
+	{
+		auth.POST("/cadastro", controllers.SignUp)
+		auth.POST("/login", controllers.Login)
+	}
+
+	usuario := router.Group("/usuario")
+	usuario.Use(middlewares.AuthMiddleware())
+	{
+		usuario.GET("/perfil", func(c *gin.Context) {
+			userID, _ := c.Get("user_id")
+			c.JSON(http.StatusOK, gin.H{"message": "Acesso permitido!", "user_id": userID})
+		})
+	}
 
 	router.Run(":8080")
 }
